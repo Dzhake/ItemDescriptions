fbfab324cedf22ff343c243d5eccd367ff6f98ed
Dzhake <andryzindmi@gmail.com>
2024-06-06 22:53:36 +0300

    Add project files.

---
+++ src/ItemDescriptions/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTitle("Item Descriptions")]
+[assembly: AssemblyCompany("|GREEN|Dzhake")]
+[assembly: AssemblyDescription("Adds descriptions for modded items")]
+[module: UnverifiableCode]
---
+++ src/ItemDescriptions/MAutoPatchHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Harmony;
+using DuckGame;
+
+namespace ItemDescriptions.PatchSystem {
+    /// <summary>
+    /// The handler that does the patching of AutoPatch attributes.
+    /// </summary>
+    public static class MAutoPatchHandler
+    {
+        /// <summary>
+        /// The patching method that finds and executes Auto-Patches.
+        /// </summary>
+        public static void Patch()
+        {
+
+            var harmony = HarmonyLoader.Loader.harmonyInstance;
+
+            foreach (var origInfo in GetAllAutoPatches())
+            {
+                if (!origInfo.IsStatic)
+                {
+                    DevConsole.Log("Skipping non-static patch method! (MAutoPatch " + origInfo.Name + " in " + origInfo.DeclaringType?.Name + ")", Color.Orange);
+                    continue;
+                }
+
+                List<MAutoPatchAttribute> attributes = origInfo.GetCustomAttributes(typeof(MAutoPatchAttribute), false).Cast<MAutoPatchAttribute>().ToList();
+
+                foreach (var attribute in attributes)
+                {
+                    MethodBase mPatch;
+
+                    if (attribute.Method is ".ctor" or "")
+                        mPatch = AccessTools.DeclaredConstructor(attribute.Type, attribute.Params);
+                    else
+                        mPatch = AccessTools.DeclaredMethod(attribute.Type, attribute.Method, attribute.Params);
+
+                    if (mPatch is null)
+                    {
+                        DevConsole.Log("Failed to find specified method: " + attribute.Method + ". on type of: " + attribute.Type.Name, Color.Red);
+                        continue;
+                    }
+
+                    switch (attribute.PatchType)
+                    {
+                        case MPatchType.Prefix:
+                            harmony.Patch(mPatch, new HarmonyMethod(origInfo));
+                            break;
+                        case MPatchType.Postfix:
+                            harmony.Patch(mPatch, null, new HarmonyMethod(origInfo));
+                            break;
+                        case MPatchType.Transpiler:
+                            harmony.Patch(mPatch, null, null, new HarmonyMethod(origInfo));
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+
+
+        }
+
+        private static IEnumerable<MethodInfo> GetAllAutoPatches()
+        {
+            return Assembly.GetExecutingAssembly().GetTypes().SelectMany(x => x.GetMethods()).Where(x =>
+                x.GetCustomAttributes(typeof(MAutoPatchAttribute), false).FirstOrDefault() != null);
+        }
+    }
+}
